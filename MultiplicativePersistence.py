b5cb72fbe13a348842fb64803d88b39c58cfe7bd
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-12-02 16:12:11 +0000

    Ruff by default

--- MultiplicativePersistence.py
+++ MultiplicativePersistence.py
@@ -8,21 +8,26 @@ from time import time
 
 # Helper Functions
 
-def tf(func, *args, **kwargs): # time func
+
+def tf(func, *args, **kwargs):  # time func
   start = time()
   r = func(*args, **kwargs)
   end = time()
   print(func.__name__, human_time(end - start))
   return r
 
+
 # Convert single digit to 2
 def digit2(x: int):
   value = x << 1
-  return "0001020304050607080910111213141516171819"\
-         "2021222324252627282930313233343536373839"\
-         "4041424344454647484950515253545556575859"\
-         "6061626364656667686970717273747576777879"\
-         "8081828384858687888990919293949596979899"[value:value+2]
+  return (
+    "0001020304050607080910111213141516171819"
+    "2021222324252627282930313233343536373839"
+    "4041424344454647484950515253545556575859"
+    "6061626364656667686970717273747576777879"
+    "8081828384858687888990919293949596979899"[value: value + 2]
+  )
+
 
 # Convert an int to a str 2 digits at a time, currently really slow bc str copies etc, do in a bytearray and use "pointer math" kinda
 def int_to_str(x: int):
@@ -36,63 +41,76 @@ def int_to_str(x: int):
     buf.append(digit2(x))
   return "".join(reversed(buf))
 
-def human_time(t: float, seconds = True): # dumb but "decently" formatted
-  return f"{int(t//60)}m {human_time((int(t)%60)+(t-int(t)), True)}" if t > 60 else f"{t:.3f}s" if t > 0.1 and seconds else f"{t*1000:.3f}ms" if t > 0.0001 else f"{t*1000000:.3f}us"
+
+def human_time(t: float, seconds=True):  # dumb but "decently" formatted
+  return (
+    f"{int(t // 60)}m {human_time((int(t) % 60) + (t - int(t)), True)}"
+    if t > 60
+    else f"{t:.3f}s"
+    if t > 0.1 and seconds
+    else f"{t * 1000:.3f}ms"
+    if t > 0.0001
+    else f"{t * 1000000:.3f}us"
+  )
+
 
 # Method demonstrated below, as a function
 # goal = how many steps
 # until = maximum length of the number in base 10
-def persistence(goal = 11, until = 64):
-  for ndigits in range(2, until + 1): # number of digits
+def persistence(goal=11, until=64):
+  for ndigits in range(2, until + 1):  # number of digits
     for front in ["26", "2", "3", "6", ""]:
       backfill = ndigits - len(front)
-      for back in combinations_with_replacement("789", backfill): # fill the rest
+      for back in combinations_with_replacement("789", backfill):  # fill the rest
         reduced = front + "".join(back)
         steps = 0
-        while len(reduced) > 1: # multiply digits together
+        while len(reduced) > 1:  # multiply digits together
           reduced = str(reduce(mul, map(int, reduced), 1))
           steps += 1
         if steps == goal:
           return f"p({goal}) = {front}{"".join(back)}"
   return f"Sorry, nothing under {until + 1} digits"
 
+
 # Above method but doing the reduce mul map int manually, so a fair bit faster
-def faststr(goal = 11, until = 64):
-  for ndigits in range(2, until + 1): # number of digits
+def faststr(goal=11, until=64):
+  for ndigits in range(2, until + 1):  # number of digits
     for front in ["26", "2", "3", "6", ""]:
       backfill = ndigits - len(front)
-      for back in combinations_with_replacement("789", backfill): # fill the rest
+      for back in combinations_with_replacement("789", backfill):  # fill the rest
         reduced = front + "".join(back)
         steps = 0
-        while len(reduced) > 1: # multiply digits together
+        while len(reduced) > 1:  # multiply digits together
           acc = 1
           for digit in reduced:
             acc *= ord(digit) - 48
-          reduced = str(acc) # 48 == ord("0")
+          reduced = str(acc)  # 48 == ord("0")
           steps += 1
         if steps == goal:
           return f"p({goal}) = {front}{"".join(back)}"
   return f"Sorry, nothing under {until + 1} digits"
 
-def faststr2(goal = 11, until = 64):
-  for ndigits in range(2, until + 1): # number of digits
+
+def faststr2(goal=11, until=64):
+  for ndigits in range(2, until + 1):  # number of digits
     for front in ["26", "2", "3", "6", ""]:
       backfill = ndigits - len(front)
-      for back in combinations_with_replacement("789", backfill): # fill the rest
+      for back in combinations_with_replacement("789", backfill):  # fill the rest
         reduced = front + "".join(back)
         steps = 0
-        while len(reduced) > 1: # multiply digits together
+        while len(reduced) > 1:  # multiply digits together
           acc = 1
           for digit in reduced:
             acc *= ord(digit) - 48
-          reduced = int_to_str(acc) # 48 == ord("0")
+          reduced = int_to_str(acc)  # 48 == ord("0")
           steps += 1
         if steps == goal:
           return f"p({goal}) = {front}{"".join(back)}"
   return f"Sorry, nothing under {until + 1} digits"
+
 
 # The same method but using integers rather than strings, gets real slow with big numbers
-def fastint(goal = 11, until = 64):
+def fastint(goal=11, until=64):
   for ndigits in range(2, until + 1):
     for front in ["26", "2", "3", "6", ""]:
       backfill = ndigits - len(front)
@@ -101,7 +119,7 @@ def fastint(goal = 11, until = 64):
         steps = 0
         while reduced > 9:
           acc = 1
-          while reduced > 0: # the slow part for big N, could try more "fixed function" divmod 10 using bitshifts
+          while reduced > 0:  # the slow part for big N, could try more "fixed function" divmod 10 using bitshifts
             reduced, mod = divmod(reduced, 10)
             # ...
             acc *= mod
@@ -110,6 +128,7 @@ def fastint(goal = 11, until = 64):
         if steps == goal:
           return f"p({goal}) = {front}{"".join(back)}"
   return f"Sorry, nothing under {until + 1} digits"
+
 
 """
 cpython 3.10.0
@@ -128,6 +147,7 @@ pypy 3.7.4 (7.3.2-alpha0)
   faststr2 3m 28.306s
 """
 
+
 def main():
   print()
   print("Generating multiplicative persistences")
@@ -135,17 +155,17 @@ def main():
   print("More info @ https://oeis.org/A003001")
   print("Source @ https://repl.it/@alexblandin/Multiplicative-Persistence")
   print()
-  
-  low = 3 # lowest persistance
-  high = 12 # 12 and higher are unknown but suspected to NOT have solutions
-  until = 200 # pypy exhausts in 43s, cpython in 2m 26s for goal=12
+
+  low = 3  # lowest persistance
+  high = 12  # 12 and higher are unknown but suspected to NOT have solutions
+  until = 200  # pypy exhausts in 43s, cpython in 2m 26s for goal=12
   for goal in range(low, high + 1):
     print(f"Finding the smallest number with persistence {goal}...")
     start = time()
-    for ndigits in range(2, until + 1): # number of digits
+    for ndigits in range(2, until + 1):  # number of digits
       for front in ["26", "2", "3", "6", ""]:
         backfill = ndigits - len(front)
-        for back in combinations_with_replacement("789", backfill): # fill the rest
+        for back in combinations_with_replacement("789", backfill):  # fill the rest
           reduced = int(f"{front}{"".join(back)}")
           steps = 0
           while reduced > 9:
@@ -156,13 +176,13 @@ def main():
             reduced = acc
             steps += 1
           if steps == goal:
-            end = time() # found it
+            end = time()  # found it
             reduced = front + "".join(back)
             print(f"{reduced} has persistence {goal}, via:")
             while len(reduced) > 1:
               reduced = str(reduce(mul, [int(digit) for digit in reduced], 1))
-              print(reduced, end = "\n" if len(reduced) > 1 else "")
-            print(f", {steps} steps found in {human_time(end-start)}\n")
+              print(reduced, end="\n" if len(reduced) > 1 else "")
+            print(f", {steps} steps found in {human_time(end - start)}\n")
             break
         else:
           continue
@@ -172,7 +192,8 @@ def main():
       break
     else:
       end = time()
-      print(f"Nothing with (up to) {until} digits for {goal} steps, used {human_time(end-start)}\n")
+      print(f"Nothing with (up to) {until} digits for {goal} steps, used {human_time(end - start)}\n")
+
 
 if __name__ == "__main__":
   # main()
